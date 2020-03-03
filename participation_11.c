5ac9397ef0c72e2bb179d31582f8eee1c4af3408
Farhan Shahbaz <49499937+Farhan12345a@users.noreply.github.com>
2020-03-02 22:22:17 -0500

    Add files via upload

---
+++ participation_11.c
@@ -0,0 +1,68 @@
+//Farhan Shahbaz
+
+//New Martinsville
+
+
+
+#include <stdio.h>
+
+
+struct ingredient
+
+{
+    
+	char name[100];
+    	int quantity;
+
+        char* measurement_type;
+
+};
+
+
+
+struct recipe
+{
+
+    char* recipe_name;
+
+    struct ingredient ingredient;
+
+};
+
+
+
+int main()
+{
+
+    printf("Hello World\n\n");
+
+    struct ingredient flour = {
+
+        .name = "Flour",
+
+        .quantity = 5,
+
+        .measurement_type = "Cup"
+
+};
+    
+
+printf("%s\n\n", flour.name);
+
+    
+    struct recipe tuna = {
+
+        "Tuna Salad",
+        
+	{"Tuna, Celery, Apples, Oranges", 10, "Cup"}
+};
+    
+    
+	printf("Recipe for %s is %s. The quantity needed is %d.", tuna.recipe_name, tuna.ingredient.name, tuna.ingredient.quantity );
+
+    
+
+	return 0;
+
+
+}
